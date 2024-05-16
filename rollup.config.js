8d1b328016f89ab17c96a35e0fb9d810740b870f
Luci-dot-exe <lucinadotexe@gmail.com>
2024-05-16 01:14:47 -0300

    Added rollup and build setup

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import typescript from "rollup-plugin-typescript2";
+
+export default {
+  input: "src/index.ts", // entry point for your application
+  output: {
+    file: "dist/bundle.js", // output bundle
+    format: "iife", // immediately-invoked function expression for browsers
+    sourcemap: true, // generate sourcemap for debugging
+  },
+  plugins: [
+    typescript({ tsconfig: "./tsconfig.json" }), // TypeScript plugin
+  ],
+};
